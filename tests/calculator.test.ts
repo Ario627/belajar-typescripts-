b1ff889c93c268bf8727fc0ea301b4ed8ceac21d
Ario627 <arioabdillah5@gmail.com>
2025-09-06 15:16:27 +0700

    coba lagi

--- tests/calculator.test.ts
+++ tests/calculator.test.ts
@@ -1,45 +1,27 @@
-import { Calculator } from '../src/calculator';
+import { Calculator } from "../src/calculator";
 
 describe('Calculator', () => {
   const calc = new Calculator();
-
-  test('add should return correct result', () => {
-    expect(calc.add(2, 3)).toEqual({
-      operation: 'penjumlahan',
+  
+  test("jumlahkan hingga benar", () => {
+    const result = calc.add(2, 5);
+    console.log(result)
+    expect(result).toEqual({
+      operation: "penjumlahan",
       a: 2,
-      b: 3,
-      result: 5
+      b: 5,
+      result: 7
     });
   });
-
-  test('substract should return correct result', () => {
-    expect(calc.substract(5, 2)).toEqual({
+  
+  test("kurangi hingga benar", () => {
+    const result = calc.substract(8, 5);
+    console.log(result);
+    expect(result).toEqual({
       operation: 'pengurangan',
-      a: 5,
-      b: 2,
+      a: 8,
+      b: 5,
       result: 3
     });
-  });
-
-  test('multiply should return correct result', () => {
-    expect(calc.multiply(4, 3)).toEqual({
-      operation: 'perkalian',
-      a: 4,
-      b: 3,
-      result: 12
-    });
-  });
-
-  test('devide should return correct result', () => {
-    expect(calc.devide(10, 2)).toEqual({
-      operation: 'pembagian',
-      a: 10,
-      b: 2,
-      result: 5
-    });
-  });
-
-  test('devide should throw error when dividing by zero', () => {
-    expect(() => calc.devide(10, 0)).toThrow("tidak bisa dibagi 0");
   });
 });
