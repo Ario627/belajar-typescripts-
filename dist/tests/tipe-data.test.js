c83a4a14f2bb2db90373358ba9f1be504ccbe2d1
Ario627 <arioabdillah5@gmail.com>
2025-09-06 05:55:58 +0000

    hasil

---
+++ dist/tests/tipe-data.test.js
@@ -0,0 +1,12 @@
+describe("Data Type", function () {
+    it('should must declare', function () {
+        const nama = "Ario Abdillah Sajad";
+        const belance = 10000000;
+        const isVip = true;
+        console.info(nama);
+        console.info(belance);
+        console.info(isVip);
+    });
+});
+export {};
+//# sourceMappingURL=tipe-data.test.js.map
